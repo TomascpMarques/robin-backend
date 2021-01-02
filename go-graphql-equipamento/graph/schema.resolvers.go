e69dced4f5f24db9dea79d76b7d056010721ecb9
tomascpmarques <tomascpmarques@gmail.com>
2021-01-02 16:11:27 +0000

    mudanças no sistema graphql

--- go-graphql-equipamento/graph/schema.resolvers.go
+++ go-graphql-equipamento/graph/schema.resolvers.go
@@ -10,11 +10,79 @@ import (
 	"go-graphql-equipamento/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) UpdateComputador(ctx context.Context, id string, input model.UpdateComputador) (*model.Computador, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+func (r *mutationResolver) UpdateGpu(ctx context.Context, id string, input model.NovoGpu) (*model.Gpu, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) UpdateCPU(ctx context.Context, id string, input model.NovoCPU) (*model.CPU, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) UpdateRAM(ctx context.Context, id string, input model.NovoRAM) (*model.RAM, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CriarComputador(ctx context.Context, input model.NovoComputador) (*model.Computador, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CriarItem(ctx context.Context, input model.NovoItem) (*model.Item, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CriarCPU(ctx context.Context, input model.NovoCPU) (*model.CPU, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CriarGpu(ctx context.Context, input model.NovoGpu) (*model.Gpu, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CriarRAM(ctx context.Context, input model.NovoRAM) (*model.RAM, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetComputadores(ctx context.Context) ([]*model.Computador, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetComputador(ctx context.Context, id string) (*model.Computador, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetCPU(ctx context.Context, id string) (*model.CPU, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetCPUS(ctx context.Context) ([]*model.CPU, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetGpu(ctx context.Context, id string) (*model.Gpu, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetGpus(ctx context.Context) ([]*model.Gpu, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetRAM(ctx context.Context, id string) (*model.RAM, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetRAMS(ctx context.Context) ([]*model.RAM, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetItems(ctx context.Context) ([]*model.Item, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetItem(ctx context.Context, id string) (*model.Item, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
