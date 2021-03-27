03cb1d88d031447d5504d371788ec03fd85a8d9b
TomascpMarques <tomascpmarques@gmail.com>
2021-03-27 00:21:22 +0000

    Mudança de uma menssagem de erro.

--- robinservicologin/loginregistohandlers/autenticacao.go
+++ robinservicologin/loginregistohandlers/autenticacao.go
@@ -86,7 +86,7 @@ func Login(user string, passwd string) map[string]interface{} {
 	utilizadorPedido, err := GetUserParaValorStruct(user)
 	if err != nil {
 		loggers.LoginAuthLogger.Println("Erro: ", err)
-		returnVal["erro"] = "Erro ao converter os dados pedidos."
+		returnVal["erro"] = "O utilisador pedido não existe."
 		return returnVal
 	}
 
