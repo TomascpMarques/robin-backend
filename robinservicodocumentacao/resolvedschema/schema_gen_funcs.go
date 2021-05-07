8d27986999b9ab0c6c78c33efa261627ae2e6d31
Tomás Marques <tomascpmarques@gmail.com>
2021-05-07 22:33:09 +0100

    auto_gen das funções de conversão de tipo.

--- robinservicodocumentacao/resolvedschema/schema_gen_funcs.go
+++ robinservicodocumentacao/resolvedschema/schema_gen_funcs.go
@@ -16,3 +16,29 @@ func RepositorioParaStruct(repo *map[string]interface{}) Repositorio {
 	}
 	return returnStruct
 }
+
+func FicheiroMetaDataParaStruct(ficheiroMeta *map[string]interface{}) FicheiroMetaData {
+	var returnStruct FicheiroMetaData
+	temp, err := json.Marshal(ficheiroMeta)
+	if err != nil {
+		return FicheiroMetaData{}
+	}
+	err = json.Unmarshal(temp, &returnStruct)
+	if err != nil {
+		return FicheiroMetaData{}
+	}
+	return returnStruct
+}
+
+func FicheiroConteudoParaStruct(ficheiroConteudo *map[string]interface{}) FicheiroConteudo {
+	var returnStruct FicheiroConteudo
+	temp, err := json.Marshal(ficheiroConteudo)
+	if err != nil {
+		return FicheiroConteudo{}
+	}
+	err = json.Unmarshal(temp, &returnStruct)
+	if err != nil {
+		return FicheiroConteudo{}
+	}
+	return returnStruct
+}
