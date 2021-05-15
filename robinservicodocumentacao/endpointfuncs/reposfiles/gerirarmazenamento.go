755b4b8c02199d98bdf9edc9519f71f14ab2b798
Tomás Marques <tomascpmarques@gmail.com>
2021-05-15 16:05:21 +0100

    func CriarFicheiro()

--- robinservicodocumentacao/endpointfuncs/reposfiles/gerirarmazenamento.go
+++ robinservicodocumentacao/endpointfuncs/reposfiles/gerirarmazenamento.go
@@ -2,7 +2,9 @@ package reposfiles
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"os"
 
 	"github.com/tomascpmarques/PAP/backend/robinservicodocumentacao/resolvedschema"
@@ -31,6 +33,39 @@ func CriarRepositorio_repo(repo *resolvedschema.Repositorio) error {
 	if criarDirerr := os.Mkdir(repo.Nome, fs.FileMode(6640)); criarDirerr != nil {
 		return criarDirerr
 	}
+
+	return nil
+}
+
+// CriarFicheiro_repo Cria o ficheiro dentro das pastas corretas no repo(dir) especificado pelos params fornecidos
+func CriarFicheiro_repo(ficheiro *resolvedschema.FicheiroMetaData) error {
+	// Repo directorie
+	os.Chdir(homePath)
+
+	// Verificar se a dir base é a correta
+	workingDir, _ := os.Getwd()
+	if !VerificarDirBase(workingDir) {
+		return errors.New("a dir base está errada")
+	}
+
+	// Walk path, cria as pastas necessárias, e muda de dir para essas
+	for _, valor := range ficheiro.Path[1 : len(ficheiro.Path)-1] {
+		if _, existe := ioutil.ReadDir("./" + valor); existe != nil {
+			if err := os.Mkdir(("./" + valor), os.FileMode(6640)); err != nil {
+				fmt.Println("Erro: ", err)
+				return errors.New("não foi possivél criar a pasta")
+			}
+		}
+		// Muda para a dir correspondente à que se encontra dentro de valor
+		os.Chdir(valor)
+	}
+
+	// Cria o ficheiro Vazio
+	file, err := os.Create(ficheiro.Nome)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
 	return nil
 }
--- robinservicodocumentacao/endpointfuncs/reposfiles/syshelpers.go
+++ robinservicodocumentacao/endpointfuncs/reposfiles/syshelpers.go
@@ -7,10 +7,10 @@ import (
 )
 
 // VerificarDirExiste Verifica se a pasta base para o repo pedido já existe
-func VerificarDirExiste(dir string, repoNome string) bool {
+func VerificarDirExiste(path string, dirNome string) bool {
 	// Verificar se o repo já existe
-	dirExiste := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() && info.Name() == repoNome {
+	dirExiste := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() && info.Name() == dirNome {
 			return errors.New("repo já existe")
 		}
 		return nil
