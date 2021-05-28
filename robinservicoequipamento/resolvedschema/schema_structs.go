e258e8b109ddea74936458fcf78bc56a4eb33e70
Tomás Marques <tomascpmarques@gmail.com>
2021-05-28 22:07:23 +0100

    Defenição da estrutura para criar um registo

--- robinservicoequipamento/resolvedschema/schema_structs.go
+++ robinservicoequipamento/resolvedschema/schema_structs.go
@@ -5,3 +5,13 @@ type Query struct {
 	Campos  map[string]interface{} `json:"campos,omitempty"`
 	Extrair [][]interface{}        `json:"extrair,omitempty"`
 }
+
+type Registo struct {
+	/* Meta do Regsito */
+	TipoRegisto string
+	Estado      string
+	Quantidade  int64
+	/* =============== */
+	// Body criado pelo o user para o registo
+	Body map[string]interface{}
+}
