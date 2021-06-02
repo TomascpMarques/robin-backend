ad2b53cbe8042372a776932feb525ec8197126e5
Tomás Marques <tomascpmarques@gmail.com>
2021-06-03 00:55:25 +0100

    Mudança da estrutura de envio de registos do query custom. Agora envia a informação pedida da mesma maneira que a func. BuscarTodosRegistosBD, envia os registos

--- robinservicoequipamento/endpointfuncs/endpointsresolvedfuncs.go
+++ robinservicoequipamento/endpointfuncs/endpointsresolvedfuncs.go
@@ -111,7 +111,9 @@ func QueryRegistoJSON(campos map[string]interface{}, colecao string, token strin
 	}
 
 	loggers.ResolverLogger.Println("Sucesso, campos extraidos com sucesso!")
-	result[colecao] = records
+	result["registos"] = map[string]interface{}{
+		colecao: records,
+	}
 	return
 }
 
